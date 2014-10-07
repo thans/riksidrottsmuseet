5236223fb81c85ee68a8245d8880b6201a8d65f8
thans <toreh@n159-p164.kthopen.kth.se>
2014-10-07 15:09:33 +0200

    initial angular setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name riksidrottsmuseetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the riksidrottsmuseetApp
+ */
+angular.module('riksidrottsmuseetApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
