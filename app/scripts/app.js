2058432515cff7dc4ea27c010ff370193a9cb9f1
thans <toreh@n161-p207.kthopen.kth.se>
2014-10-16 17:43:57 +0200

    Added gifs to detail page and changed color scheme

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,83 +33,114 @@ angular.module('riksidrottsmuseetApp', [
         number: '1',
         name: 'Jacob',
         heartRate: 100,
-        steps: 300
+        steps: 300,
+        throwing: 45,
+        reaction: 0.65
     }, {
         number: '2',
         name: 'Marie',
         heartRate: 89,
-        steps: 482
+        steps: 482,
+        throwing: 51,
+        reaction: 0.39
     }, {
         number: '3',
         name: '-',
         heartRate: 67,
-        steps: 135
+        steps: 135,
+        throwing: 37,
+        reaction: 0.81
     }, {
         number: '4',
         name: '-',
         heartRate: 132,
-        steps: 576
+        steps: 576,
+        throwing: 68,
+        reaction: 0.76
     }, {
         number: '5',
-        name: 'Dave',
+        name: 'Emma',
         heartRate: 94,
-        steps: 1139
+        steps: 1139,
+        throwing: 44,
+        reaction: 0.28
   },{
         number: '6',
-        name: 'Jacob',
+        name: 'Daniel',
         heartRate: 106,
-        steps: 328
+        steps: 328,
+        throwing: 72,
+        reaction: 0.46
     }, {
         number: '7',
-        name: 'Marie',
+        name: 'Elin',
         heartRate: 96,
-        steps: 467
+        steps: 467,
+        throwing: 73,
+        reaction: 0.53
     }, {
         number: '8',
         name: '-',
         heartRate: 103,
-        steps: 342
+        steps: 342,
+        throwing: 38,
+        reaction: 0.51
     }, {
         number: '9',
         name: '-',
         heartRate: 76,
-        steps: 672
+        steps: 672,
+        throwing: 91,
+        reaction: 0.44
     }, {
         number: '10',
-        name: 'Dave',
+        name: 'Elvira',
         heartRate: 111,
-        steps: 987
+        steps: 987,
+        throwing: 65,
+        reaction: 0.46
   },{
         number: '11',
-        name: 'Jacob',
+        name: 'Robin',
         heartRate: 132,
-        steps: 274
+        steps: 274,
+        throwing: 61,
+        reaction: 0.28
     }, {
         number: '12',
-        name: 'Marie',
+        name: 'Sofie',
         heartRate: 98,
-        steps: 167
+        steps: 167,
+        throwing: 72,
+        reaction: 0.31
     }, {
         number: '13',
         name: '-',
         heartRate: 67,
-        steps: 135
+        steps: 135,
+        throwing: 48,
+        reaction: 0.5
     }, {
         number: '14',
         name: '-',
         heartRate: 132,
-        steps: 576
+        steps: 576,
+        throwing: 49,
+        reaction: 0.6
     }, {
         number: '15',
-        name: 'Dave',
-        heartRate: 160,
-        steps: 900
+        name: 'Tore',
+        heartRate: 76,
+        steps: 50,
+        throwing: 54,
+        reaction: 0.8
   }];
 
   setInterval(function() {
     for (var i = 0; i < $rootScope.users.length; i++) {
       $rootScope.users[i].steps += Math.floor(Math.random() * 11) + 1;
     }
+
     $rootScope.$apply();
   }, 5000);
 
@@ -121,6 +152,8 @@ angular.module('riksidrottsmuseetApp', [
       }
       addRate *= Math.floor(Math.random() * 4) + 1;
       $rootScope.users[i].heartRate += addRate;
+      $rootScope.users[i].heartRate = Math.max($rootScope.users[i].heartRate, 64);
+      $rootScope.users[i].heartRate = Math.min($rootScope.users[i].heartRate, 180);
     }
     $rootScope.$apply();
   }, 15000);
